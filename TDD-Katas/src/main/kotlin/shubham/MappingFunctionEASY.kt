0264d72fc0067def052d077fbeddfcfc6b5951b5
shubham9237 <l.shubham9237@gmail.com>
2025-02-28 10:47:43 +0530

    [TDD] Added new easy problem in JS

---
+++ TDD-Katas/src/main/kotlin/shubham/MappingFunctionEASY.kt
@@ -0,0 +1,21 @@
+package org.example.shubham
+
+class MappingFunctionEASY {
+
+    /**
+     * @param {number[]} arr
+     * @param {Function} fn
+     * @return {number[]}
+     */
+// Problem was solved in JS below is the code
+//    var map = function(arr, fn) {
+//        const convertedArr = []
+//        for(let i=0;i<arr.length;i++){
+//        const params=[arr[i], i]
+//        const newElm = fn(...params)
+//        convertedArr.push(newElm)
+//    }
+//        return convertedArr;
+//    };
+
+}
